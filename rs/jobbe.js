d1813bd6c17e94f606249adc6f76179e60cbce49
M0skus <85843306+M0skus@users.noreply.github.com>
2021-12-08 17:51:57 +0100

    Add files via upload

---
+++ rs/jobbe.js
@@ -0,0 +1,16 @@
+
+
+function produkter() {
+    window.location.href = "file:///C:/programering/Lullise%20programmer/Business/produkter.html";
+}
+
+function drikke() {
+    window.location.href = "https://www.vinmonopolet.no/search?q=:relevance:visibleInSearch:true:mainCategory:brennevin&searchType=product";
+}
+function home() {
+    window.location.href = "file:///C:/programering/Lullise%20programmer/Business/main.html";
+}
+
+function cart() {
+    window.location.href = "file:///C:/programering/Lullise%20programmer/Business/vipps.html";
+}
